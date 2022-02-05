b133975f29351f13b9d5a52cadc7f7019365939f
Julien <julien.auclair93@gmail.com>
2022-02-05 15:05:37 +0100

    encode query

--- src/services/api/search/index.js
+++ src/services/api/search/index.js
@@ -2,5 +2,5 @@ import axios from "axios";
 import { baseURL } from "..";
 
 export const getBooks = async (q, page, re) => {
-    return axios.get(`${baseURL}/search?q=${q}&page=${page}&re=${re}`);
-}
+    return axios.get(`${baseURL}/search?q=${encodeURIComponent(q)}&page=${page}&re=${re}`);
+};
